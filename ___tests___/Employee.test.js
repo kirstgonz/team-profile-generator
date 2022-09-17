afdbcab0d5a12c8d69df609a2e5697f825665252
kirstgonz <104031234+kirstgonz@users.noreply.github.com>
2022-09-17 16:46:48 -0500

    updated employee.js test to pass

--- ___tests___/Employee.test.js
+++ ___tests___/Employee.test.js
@@ -1,3 +1,10 @@
-name,id,email
-
 const Employee = require('../lib/Employee')
+
+test('makes an Employee object', () => {
+    const employee = new Employee ('Kirstyn', '1', 'kcg@yahoo.com');
+
+    expect(employee.name).toBe('Kirstyn');
+    expect(employee.id).toBe('1');
+    expect(employee.email).toBe('kcg@yahoo.com')
+
+});
